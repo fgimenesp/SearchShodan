78bb82bfa1c9056552ab8a4600fa5525c4567287
root <root@localhost.localdomain>
2017-12-04 11:07:30 -0200

    ShodanSearchIncomunPorts

---
+++ shodansearch_incomunsport.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+#Create by /x46/x47/x50
+
+import shodan
+
+SHODAN_API_KEY = "YOUR API KEY"
+
+api = shodan.Shodan(SHODAN_API_KEY)
+
+try:
+            # This is a script that aims to find common ports in any AS
+            results = api.search('ASN:Your AS -port:25,80,110,143,443,500,4500,8080,8443')
+ 
+            # Show the results
+            print 'Results found: %s' % results['total']
+            for result in results['matches']:
+                   print 'IP: %s' % result['ip_str']
+                   print 'Port: %s' %result['port']
+                   print result['data']
+                   print ''
+except shodan.APIError, e:
+            print 'Error: %s' % e
+
